5be28871f44c7055304cb3524b84ac0140e4420c
Jeancarlos Herrera <jeancarlos.herrera113@gmail.com>
2022-12-13 17:31:04 -0400

    Making changes

--- tests.py
+++ tests.py
@@ -3,4 +3,4 @@ from screenhandlerpackage import Operation
 result = Operation.addition(2, 10)
 result2 = Operation.division(10, 2)
 
-print (result, result2);
+print ("Addition result: ", result, " and division result: ", result2);
